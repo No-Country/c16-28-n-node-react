f885f307f26c045200900250ae4b330d0bb8c911
Humberto Colmenares <colmenareshr@gmail.com>
2024-02-21 23:56:54 -0300

    adicionando colores, tamaño de tipografia y peso

--- Front/tailwind.config.js
+++ Front/tailwind.config.js
@@ -2,8 +2,28 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'sky-blue': '#76B4CC',
+      orange: '##FF6F00',
+      yellow: '#FFA000',
+      background: '#EEEEEE',
+      dark: '#1C1A1A',
+      black: '#211A1A',
+      gray: '#3E3B3B',
+    },
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
+    },
+    fontSize: {
+      title: '1.375rem',
+      subtitle: '1.125rem',
+      text: '0.875rem',
+    },
+    fontWeight: {
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
     },
     extend: {},
   },
