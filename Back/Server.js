2d03aab55bfe7c7f2a61605272797021e77ee8d9
Bárbara Espinola <Baesp2023@gmail.com>
2024-02-22 20:04:56 -0300

    created model ProvService
    Cambie la relación entre proveedores y servicios , cree un modelo nuevo ,y cambie unos controllers y rutas (ya lo actualice en la doc del front "RutasDelBack"

--- Back/Server.js
+++ Back/Server.js
@@ -37,7 +37,7 @@ server.use((err, req, res, next) => {
 
 // Server On:
     console.log('Iniciando la aplicación');
-conn.sync({force:false})
+conn.sync({alter:false})
   .then(() => {
     server.listen(port, async () => {
       console.log('Servidor ON in http://localhost:3001/');
