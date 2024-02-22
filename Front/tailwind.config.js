016376d196db0d7c7d09c914bda677b137af3ef4
Humberto Colmenares <colmenareshr@gmail.com>
2024-02-22 02:30:37 -0300

    agregando tamaños a tipografia

--- Front/tailwind.config.js
+++ Front/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
   theme: {
     colors: {
       skyBlue: '#76B4CC',
+      blue: '#0D47A1',
       orange: '#FF6F00',
       yellow: '#FFA000',
       background: '#EEEEEE',
@@ -18,6 +19,16 @@ export default {
       title: '1.375rem',
       subtitle: '1.125rem',
       text: '0.875rem',
+      sm: '.8rem',
+      base: '1rem',
+      lg: '1.2rem',
+      xl: '1.4rem',
+      '2xl': '1.6rem',
+      '3xl': '1.8rem',
+      '4xl': '2rem',
+      '5xl': '2.2rem',
+      '6xl': '2.4rem',
+      '7xl': '2.6rem',
     },
     fontWeight: {
       normal: '400',
