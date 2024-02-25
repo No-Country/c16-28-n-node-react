3b34bbd20faf14b65d18ea222395f93c1defd477
Bárbara Espinola <Baesp2023@gmail.com>
2024-02-25 13:39:32 -0300

    update Front

--- Front/src/store/auth.js
+++ Front/src/store/auth.js
@@ -1,17 +1,17 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+// import { persist } from 'zustand/middleware';
 
 export const useAuthStore = create(
-  persist(
-    (set) => ({
-      token: null,
-      setToken: (token) =>
-        set((state) => ({
-          token,
-        })),
-    }),
-    {
-      name: 'auth',
-    }
-  )
+  // persist(
+  //   (set) => ({
+  //     token: null,
+  //     setToken: (token) =>
+  //       set((state) => ({
+  //         token,
+  //       })),
+  //   }),
+  //   {
+  //     name: 'auth',
+  //   }
+  // )
 );
