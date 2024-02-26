304190f67350fcd4da2ee3a412f23da0467586ee
Humberto Colmenares <colmenareshr@gmail.com>
2024-02-25 21:16:14 -0300

    agregar logo en toda la aplicacion

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/index.jsx';
+import NavBar from './components/NavBar/NavBar.jsx';
 
 const App = () => {
   return (
     <BrowserRouter>
       <main className='container'>
+        <header>
+        <NavBar />
+        </header>
         <AppRoutes />
       </main>
     </BrowserRouter>
