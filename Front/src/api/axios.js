6db952d34d116244f3a96fc272d0bd68e3db376a
Humberto Colmenares <colmenareshr@gmail.com>
2024-02-25 21:05:20 -0300

    configurando axios

--- Front/src/api/axios.js
+++ Front/src/api/axios.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
-import { useAuthStore } from '../store/auth';
 
 const baseURL = 'http://localhost:3001';
-const api = axios.create({
-  baseURL,
-  withCredentials: true,
-});
 
-api.interceptors.request.use((config) => {
-  const token = useAuthStore.getState().token;
-  config.headers = {
-    Authorization: `Bearer ${token}`,
-  };
-  return config;
-});
+const configureAxios = (token) => {
+  const api = axios.create({
+    baseURL,
+    withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return api;
+};
 
-export default api;
+export default configureAxios;
