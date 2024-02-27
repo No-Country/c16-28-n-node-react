0037dcf0b93bfc4c322e557470bf76a1f17e4c25
Humberto Colmenares <colmenareshr@gmail.com>
2024-02-27 00:06:38 -0300

    Agregando funcionalidad de Login

--- Front/src/pages/Login/Login.jsx
+++ Front/src/pages/Login/Login.jsx
@@ -1,21 +1,26 @@
+import { Link } from "react-router-dom"
 import LoginForm from "../../components/LoginForm/LoginForm"
-import NavBar from "../../components/NavBar/NavBar"
 
 const Login = () => {
   return (
-    <div className="px-6">
-          <div className="flex flex-col gap-8">
-            <h1>Inicia sesión</h1>
-            <LoginForm />
-            <div className="w-full flex justify-center items-center">
-              <div className="w-full h-px bg-black mx-5"></div>
-              <span className="text-text">o</span>
-              <div className="w-full h-px bg-black mx-5"></div>
-            </div>
-            <button className="w-full p-3 rounded border text-text font-medium text-dark ">Regístrate</button>
-          </div>
+    <div className='px-6'>
+      <div className='flex flex-col gap-8'>
+        <h1>Inicia sesión</h1>
+        <LoginForm />
+        <div className='w-full flex justify-center items-center'>
+          <div className='w-full h-px bg-black mx-5'></div>
+          <span className='text-text'>o</span>
+          <div className='w-full h-px bg-black mx-5'></div>
+        </div>
+        <Link
+          to={'/register'}
+          className='secondaryBtn'
+        >
+          Regístrate
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login
