5c9f3cd0cacb741d417af7982dea36488242cbed
Humberto Colmenares <colmenareshr@gmail.com>
2024-02-26 23:00:50 -0300

    resolviendo problemas de key repetidas

--- Front/src/pages/DataProvedores/perfildeProv.jsx
+++ Front/src/pages/DataProvedores/perfildeProv.jsx
@@ -4,56 +4,62 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const Proveedores = () => {
-
   const { provider, loadprovider } = providerStore();
   const { id_prov } = useParams();
 
   useEffect(() => {
     loadprovider(id_prov);
-  }, [id_prov,  loadprovider]);
+  }, [id_prov, loadprovider]);
 
-    return (
-      <div >
-        <hr className='h-0.5 bg-blue border-0'/>
-          {provider && (
-            <ul>
-            <li key={id_prov} className='items-left justify-center p-[5%] ml-1 mb-2'>
-              <h2 className='text-xl'> Datos:</h2> <br/>
-          <img className='float-right h-20 max-w-[6.9em] rounded-lg ml-1' src={`${provider.img}`} alt="photo"></img>
-              Nombre:<h2 className='ml-5'>{provider.name}</h2>
-              Apellido:<h2 className='ml-5'> {provider.lastName}</h2>
-    </li>
-    <hr className='h-0.5 bg-blue border-0'/>
-    <li key={id_prov} className='items-left justify-center p-[5%] ml-1 mb-2 '>
-            <h2>Disponibilidad horaria: </h2>
-            <h2 style={{ fontSize: '0.9em' } } className='ml-5'>{provider.horary}</h2>
-    </li>
-    <hr className='h-0.5 bg-blue border-0'/>
-    <li key={id_prov} className='items-left justify-center p-[5%] ml-1 mb-2 '>
-            <h2>Certificaciones: </h2>
-            <h2 style={{ fontSize: '0.9em' }} className='ml-5'>{provider.horary}</h2>
-    </li>
-    <hr className='h-0.5 bg-blue border-0'/>
-    <li key={id_prov} className='items-left justify-center p-[5%] ml-1 mb-2 '>
-            <h2>Trabajos realizados: </h2>
-            <h2 style={{ fontSize: '0.9em' }} className='ml-5'> Sin fotos</h2>
-    </li>
-    <hr className='h-0.5 bg-blue border-0'/>
-    <li key={id_prov} className='items-left justify-center p-[5%] ml-1 mb-2 '>
-            <h2>Reseñas: </h2>
-            <h2 style={{ fontSize: '0.9em' }} className='ml-5'> Sin Reseñas</h2>
-    </li>
-    <hr className='h-0.5 bg-blue border-0'/><br/>
-            </ul>
-          )}
+  return (
+    <div>
+      <hr className='h-0.5 bg-blue border-0' />
+      {provider && (
+        <ul key={id_prov}>
+          <li className='items-left justify-center p-[5%] ml-1 mb-2'>
+            <h2 className='text-xl'> Datos:</h2> <br />
+            <figure className='float-right h-20 max-w-[6.9em] ml-1'>
+              <img
+                className='rounded-lg'
+                src={`${provider.img}`}
+                alt={`${provider.name}`}
+              />
+            </figure>
+            Nombre:<h2 className='ml-5'>{provider.name}</h2>
+            Apellido:<h2 className='ml-5'> {provider.lastName}</h2>
+          </li>
+          <hr className='h-0.5 bg-blue border-0' />
+          <li className='items-left justify-center p-[5%] ml-1 mb-2 '>
+            <p className='p__h3'>Disponibilidad horaria: </p>
+            <p className='p__title'>{provider.horary}</p>
+          </li>
+          <hr className='h-0.5 bg-blue border-0' />
+          <li className='items-left justify-center p-[5%] ml-1 mb-2 '>
+            <p className='p__h3'>Certificaciones: </p>
+            <p className='p__title'>{provider.horary}</p>
+          </li>
+          <hr className='h-0.5 bg-blue border-0' />
+          <li className='items-left justify-center p-[5%] ml-1 mb-2 '>
+            <p className='p__h3'>Trabajos realizados: </p>
+            <p className='p__title'> Sin fotos</p>
+          </li>
+          <hr className='h-0.5 bg-blue border-0' />
+          <li className='items-left justify-center p-[5%] ml-1 mb-2 '>
+            <p className='p__h3'>Reseñas: </p>
+            <p className='p__title'> Sin Reseñas</p>
+          </li>
+          <hr className='h-0.5 bg-blue border-0' />
+          <br />
+        </ul>
+      )}
 
-      <div className="flex justify-center">
-        <Link to="/contactRequest">
-        <button className='bg-orange px-4 py-2 hover:bg-blue'>Solicitar Contacto</button>
+      <div className='flex justify-center pb-8'>
+        <Link to='/contactRequest'>
+          <button className='primaryBtn'>Solicitar Contacto</button>
         </Link>
       </div>
-</div>
-    )
-  }
-  
-  export default Proveedores
+    </div>
+  );
+};
+
+export default Proveedores;
