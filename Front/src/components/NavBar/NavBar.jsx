d207eea2cd0e0af71ac801dce716dc376dc6bfe9
Humberto Colmenares <colmenareshr@gmail.com>
2024-03-04 21:18:47 -0300

    limpiando código

--- Front/src/components/NavBar/NavBar.jsx
+++ Front/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import useUserStore from '../../store/auth';
 import { toast } from 'react-hot-toast';
 import Sidebar from '../Sidebar/Sidebar';
