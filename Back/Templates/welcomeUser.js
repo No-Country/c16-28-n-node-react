3607dff451f37f872b389cc7d7f7d1303a0de75b
Bárbara Espinola <Baesp2023@gmail.com>
2024-03-04 22:32:28 -0300

    changes

--- Back/Templates/welcomeUser.js
+++ Back/Templates/welcomeUser.js
@@ -6,7 +6,7 @@ function welcomeUserTemplate(name, lastName, email) {
   <p style="color: #666;"><strong>${name} ${lastName}</strong>, estamos emocionados de tenerte como parte de nuestra comunidad. Tu registro con el correo <strong>${email}</strong> ha sido exitoso.</p>
   <p style="color: #666;">Ya puedes completar tu perfil y empezar a utilizar nuestra plataforma para conectar con proveedores de servicios.</p>
   <h2>¡Muchas gracias por elegirnos!</h2>
-  <a href="#" style="display: inline-block; background-color: #007bff; color: #fff; text-decoration: none; padding: 10px 20px; margin-top: 20px; border-radius: 5px;">Completar perfil</a>
+  <a href="https://dev.serviapp.solutions/usuario/perfil" style="display: inline-block; background-color: #007bff; color: #fff; text-decoration: none; padding: 10px 20px; margin-top: 20px; border-radius: 5px;">Completar perfil</a>
   <img src="https://res.cloudinary.com/dq9icw8vb/image/upload/v1709388024/mczmuz3tirvjkm1cvtjg.png" alt="" style="display: block; margin: 0 auto; max-width: 100%; height: auto;">
   <hr style="border: 1px solid #0D47A1;">
   <p style="color: #666;">Si tienes alguna pregunta o necesitas ayuda, no dudes en ponerte en contacto con nuestro equipo de soporte.</p>
